c2ee4a69fa6846e43387c5c6e32d546abb0ea772
kapustekk <tomasz.kapuscinski.52@gmail.com>
2020-10-22 15:12:23 +0200

    seminarium 3 program1 test

--- sem3/p1.c
+++ sem3/p1.c
@@ -6,8 +6,10 @@ int main(int argc, char*argv[]){
     float v;
     float a1;
     float a2;
-    a1=atof(argv[1]);
-    a2=atof(argv[2]);
+    //a1=atof(argv[1]);
+    //a2=atof(argv[2]);
+    a1=atof("12.3");
+    a2=atof("0.4");
     v=a1+a2;
     printf("[%s] Suma: %f\n",argv[0], v);
 }
