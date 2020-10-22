9164ad36162f0c5410502a3b3a4011a2ff138c07
kapustekk <tomasz.kapuscinski.52@gmail.com>
2020-10-22 16:59:30 +0200

    zadanie domowe 1 seminarium 3

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -6,7 +6,7 @@ float kwadrat;
 float argument;
 argument=atof(argv[1]);
 kwadrat=argument*argument;
-printf("Wartość %f do kwardratu to %f\n",argument ,kwadrat);
+printf("Wartość %f do kwardratu to %f.\n",argument ,kwadrat);
 //  jeśli chciałbym przybliżyć wartość np do jednej cyfry po przecinkuu napisałbym:
 //      printf("Wartość %.1f do kwardratu to %.1f\n",argument ,kwadrat);
 }
